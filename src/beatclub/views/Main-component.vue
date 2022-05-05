fc1e6ebf41f7cf13356263078905bac704f5a3f9
Diego <diegotasa35@hotmail.com>
2022-05-05 13:24:13 -0500

    main fixed css styles

--- src/beatclub/views/Main-component.vue
+++ src/beatclub/views/Main-component.vue
@@ -1,39 +1,40 @@
 <template>
   <div class="container">
-
-    <v-app-bar
-        color="deep-purple accent-4"
-        dense
-        dark
-    >
+    <v-app-bar  color="deep-purple accent-4"
+                dense
+                dark>
       <!--      <v-app-bar-nav-icon></v-app-bar-nav-icon>-->
       <v-toolbar-title >
+        <v-btn>
           <v-img
-              style="height: 50px; width: 100px"
-              src="https://github.com/BitForce-BeatClub/LandingPage/blob/main/img/logo.png?raw=true"
+            style="height: 50px; width: 100px"
+            src="https://github.com/BitForce-BeatClub/LandingPage/blob/main/img/logo.png?raw=true"
           ></v-img>
 
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn plain to="" color="white">
-        Top Producers
+        Explore
       </v-btn>
       <v-btn plain to="" color="white">
-        Advertisements
-      </v-btn>
-      <v-btn plain to="" color="white">
-        Profile
-        <v-icon>mdi-account</v-icon>
+        Creator
       </v-btn>
       <v-btn plain to="/Login" color="white">
         Sign Up │Sign in
       </v-btn>
-    </v-app-bar>
+      <v-btn to="/Login" color="red">
+        Star Collaborating
+      </v-btn>
 
+    </v-app-bar>
     <div class="img-container">
-      <img class="img-portada" src="src/assets/portada.png" alt="">
+      <h1>the <span style="color: #D90429;">world’s #1</span>  platform for<br> music production</h1>
     </div>
     <div style="text-align: left; margin-top: 20px" >
       <h1>Recommendations</h1>
@@ -52,12 +53,26 @@ export default {
   margin-top: 20px;
   /*border: 2px solid red;*/
 }
-.img-portada{
-  height: 300px;
-  width: 100%;
+.img-container{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  font-family: 'Bebas Neue';
+  height: 45vh;
+  background-image: linear-gradient(
+    0deg,
+    rgba(0,0,0,0.1),
+    rgba(0,0,0,0.1)
+  )
+  ,url(src/assets/background.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center ;
+}
+.img-container h1{
+  margin-left: 2rem;
+  font-size: 4rem;
+  text-align: left;
 }
 
-.img-container{
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-}
 </style>
